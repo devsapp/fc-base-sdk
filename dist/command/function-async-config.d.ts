1f4c29362d6c7c3831426a613c9659aa32e57192
千风 <qianfeng.zqf@alibaba-inc.com>
2021-08-12 16:21:36 +0800

    feat: version 0.0.17

--- dist/command/function-async-config.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function makeDestination({ serviceName, functionName, asyncConfiguration, qualifier, }: {
-    serviceName: any;
-    functionName: any;
-    asyncConfiguration: any;
-    qualifier?: string;
-}): Promise<void>;
